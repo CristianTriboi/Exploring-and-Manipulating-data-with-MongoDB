9c62564f471755d61620a894a371b0e883c719b8
Cristian <51482096+CristianTriboi@users.noreply.github.com>
2025-08-23 01:32:41 +0200

    Create app_list.js

---
+++ app_list.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Employee = require('./employee');
+
+const uri =  "mongodb://root:psw@host:27017";
+
+mongoose.connect(uri,{'dbName':'employeeDB'});
+
+Employee.find().then((data)=>{
+            console.log(data);
+            mongoose.connection.close()
+        })
